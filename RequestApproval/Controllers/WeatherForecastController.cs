4791305890eccf8c5e3f857fb1bbfa1cb0d98d7b
Ibrahim Ragab <ibrahim.ragab.net@gmail.com>
2023-05-05 03:47:56 +0300

    Add Sample2

---
+++ RequestApproval/Controllers/WeatherForecastController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using ResumableFunctions.Handler;
+using ResumableFunctions.Handler.Attributes;
+using ResumableFunctions.Handler.InOuts;
+
+namespace RequestApproval.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RequestApprovalController : ControllerBase
+    {
+        private readonly ILogger<RequestApprovalController> _logger;
+        private readonly RequestApprovalService service;
+
+        public RequestApprovalController(ILogger<RequestApprovalController> logger, RequestApprovalService service)
+        {
+            _logger = logger;
+            this.service = service;
+        }
+
+        [HttpPost(nameof(UserSubmitRequest))]
+        public bool UserSubmitRequest(Request request)
+        {
+            return service.UserSubmitRequest(request);
+        }
+
+    }
+    public class RequestApprovalWorkflow : ResumableFunction
+    {
+        private readonly RequestApprovalService service;
+
+        public RequestApprovalWorkflow()
+        {
+            //passing dependancies in constructor have a bug that I work on
+            service = CurrentServiceProvider.GetService<RequestApprovalService>();
+        }
+
+        public Request UserRequest { get; set; }
+
+        [ResumableFunctionEntryPoint("RequestApprovalWorkflow.RequestApprovalFlow")]
+        internal async IAsyncEnumerable<Wait> RequestApprovalFlow()
+        {
+            yield return WaitUserSubmitRequest();
+            Console.WriteLine("After User Submit Request");
+        }
+
+        private Wait WaitUserSubmitRequest()
+        {
+            return Wait<Request, bool>("Wait User Submit Request", service.UserSubmitRequest)
+                             .MatchIf((request, result) => request.Id > 0)
+                             .SetData((request, result) => UserRequest == request);
+        }
+    }
+    public class RequestApprovalService
+    {
+        public RequestApprovalService()
+        {
+
+        }
+        [WaitMethod("RequestApproval.UserSubmitRequest")]
+        public bool UserSubmitRequest(Request request)
+        {
+            return true;
+        }
+    }
+    public class Request
+    {
+        public int Id { get; set; }
+        public string SomeContent { get; set; }
+    }
+}
---
+++ RequestApproval/Program.cs
@@ -0,0 +1,33 @@
+using RequestApproval.Controllers;
+using ResumableFunctions.AspNetService;
+using ResumableFunctions.Handler.InOuts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services
+    .AddControllers()
+    .AddResumableFunctions(new ResumableFunctionsSettings().UseSqlServer());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<RequestApprovalService>();
+builder.Services.AddTransient<RequestApprovalWorkflow>();
+
+var app = builder.Build();
+app.ScanCurrentService();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
